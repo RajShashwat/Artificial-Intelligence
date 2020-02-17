1ec25206f9c4bc850a8ca0dd37cb8675ab6d3957
Shashwat Raj <shashwat.r2018bcs@srisriuniversity.edu.in>
2020-02-17 20:49:28 +0530

    Comment Added

--- Prologue/Tower_of_hanoi.pl
+++ Prologue/Tower_of_hanoi.pl
@@ -1,9 +1,15 @@
+ / * Coded by Shashwat Raj
+Prologue program of solving Tower-of-Hanoi*/
+
+/* When exactly one disk is there in Tower of Hanoi*/
 move(1,X,Y,_) :-
     write('Move top disk from '),
     write(X),
     write(' to '),
     write(Y),
     nl.
+
+/* When N no. of disk are there in Tower of Hanoi*/
 move(N,X,Y,Z) :-
   N>1,
   M is N-1,
